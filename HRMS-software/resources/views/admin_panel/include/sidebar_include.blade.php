5139d0ab7d18a8b36aa9f1ed871e99a1a30abd0b
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-05-16 01:03:48 +0500

    Employee Attendance Push

--- HRMS-software/resources/views/admin_panel/include/sidebar_include.blade.php
+++ HRMS-software/resources/views/admin_panel/include/sidebar_include.blade.php
@@ -4,7 +4,7 @@
  <div class="deznav">
      <div class="deznav-scroll">
          <ul class="metismenu" id="menu">
-             <li><a href="{{ route('admin-dashboard') }}" aria-expanded="false">
+             <li><a href="{{ route('home') }}" aria-expanded="false">
                      {{-- <i class="flaticon-381-networking"></i> --}}
                      <i class="fa-solid fa-house"></i>
                      <span class="nav-text">Dashboard</span>
--- HRMS-software/resources/views/employee_panel/include/sidebar_include.blade.php
+++ HRMS-software/resources/views/employee_panel/include/sidebar_include.blade.php
@@ -17,18 +17,28 @@
                  </a>
              </li>
 
-                 <li>
-                     <a href="{{ route('employee-cmr') }}" aria-expanded="false">
-                         <i class="fas fa-boxes"></i>
-                         <span class="nav-text">CMR</span>
-                     </a>
-                 </li>
-                 <li>
-                    <a href="{{ route('mytask') }}" aria-expanded="false">
-                        <i class="fas fa-book-open"></i>
-                        <span class="nav-text">My Task</span>
-                    </a>
-                </li>
+             <li>
+                 <a href="{{ route('employee-cmr') }}" aria-expanded="false">
+                     <i class="fas fa-boxes"></i>
+                     <span class="nav-text">CMR</span>
+                 </a>
+             </li>
+
+             <li class="has-menu"><a class="has-arrow ai-icon" href="#" aria-expanded="false">
+                     <i class="fas fa-check-circle"></i>
+                     <span class="nav-text">Attendance</span>
+                 </a>
+                 <ul aria-expanded="false">
+                     <li><a href="{{ route('employee-attendance-create') }}">Creae Attendance</a></li>
+                     <li><a href="{{ route('all-employee-attendance') }}">Check Attendance</a></li>
+                 </ul>
+             </li>
+             <li>
+                 <a href="{{ route('mytask') }}" aria-expanded="false">
+                     <i class="fas fa-book-open"></i>
+                     <span class="nav-text">My Task</span>
+                 </a>
+             </li>
          </ul>
 
      </div>
