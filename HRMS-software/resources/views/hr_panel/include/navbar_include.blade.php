b786a27176ea60ae3d1a18f758b118b1cddc50cf
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-05-24 12:31:58 +0500

    change

--- HRMS-software/resources/views/hr_panel/include/navbar_include.blade.php
+++ HRMS-software/resources/views/hr_panel/include/navbar_include.blade.php
@@ -51,7 +51,7 @@
                                 @endif
                             </a>
                             <div class="dropdown-menu dropdown-menu-end">
-                                <a href="{{ route('hr-profile-page') }}" class="dropdown-item ai-icon">
+                                <a href="#" class="dropdown-item ai-icon">
                                     <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary"
                                         width="18" height="18" viewBox="0 0 24 24" fill="none"
                                         stroke="currentColor" stroke-width="2" stroke-linecap="round"
