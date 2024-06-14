a58c7a5986be223075c3aaaa6b132ea41b67899d
Kashan Shaikh <96263043+kashan232@users.noreply.github.com>
2024-06-15 01:35:18 +0800

    changes

--- HRMS-software/routes/web.php
+++ HRMS-software/routes/web.php
@@ -53,7 +53,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// git connect
+// git connect on pc
 //kashan pagal he
 Route::get('/', function () {
     return view('welcome');
